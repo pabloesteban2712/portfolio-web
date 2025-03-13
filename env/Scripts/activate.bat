2d93e36d9a60286c4050b899ff2ad2b24de2cedd
Pablo <estebangagopablo7@gmail.com>
2025-03-13 22:27:57 +0100

    change deploy server

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\poulv\Desktop\PORTFOLIO\portfolio-web\web-portfolio\env
+set VIRTUAL_ENV=C:\Users\poulv\Desktop\CONDITIONAL PROJECTS\2 DA MOON\COURSES\DJANGO CURSO\PORTFOLIO\portfolio-web\env
 
 if not defined PROMPT set PROMPT=$P$G
 
